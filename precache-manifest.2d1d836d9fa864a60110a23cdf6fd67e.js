d8dc4432a98af8d98878b52eb7f9d00e5b9dc8cf
bay <bayram.hendem@qmarketing.de>
2020-03-09 10:21:18 +0100

    Updates

---
+++ precache-manifest.2d1d836d9fa864a60110a23cdf6fd67e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a65f3e731701e6d7d99e052afa10206",
+    "url": "./index.html"
+  },
+  {
+    "revision": "614fb366311943d92bef",
+    "url": "./static/css/main.ed0f25ef.chunk.css"
+  },
+  {
+    "revision": "afe285991783503e6d2d",
+    "url": "./static/js/2.61497dd3.chunk.js"
+  },
+  {
+    "revision": "04e8208326670d6da2ddd96291b4d26f",
+    "url": "./static/js/2.61497dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614fb366311943d92bef",
+    "url": "./static/js/main.8e5f1c5b.chunk.js"
+  },
+  {
+    "revision": "136a2f3168f80faec449",
+    "url": "./static/js/runtime-main.298f06da.js"
+  },
+  {
+    "revision": "f132e4ffd7ac6324716962c1459d459b",
+    "url": "./static/media/SFProDisplay-BlackItalic.f132e4ff.ttf"
+  }
+]);
